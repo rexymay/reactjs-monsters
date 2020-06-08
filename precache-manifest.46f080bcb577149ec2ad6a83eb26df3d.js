054285355b3107c011cf5fa0aa1730eb8fbb2751
Rex Sacayan <rex.sacayan@g.msuiit.edu.ph>
2020-06-09 01:45:48 +0800

    Updates

---
+++ precache-manifest.46f080bcb577149ec2ad6a83eb26df3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ef55492fa542a43734230174eb398e5",
+    "url": "/reactjs-monsters/index.html"
+  },
+  {
+    "revision": "00f54dddca48188b9624",
+    "url": "/reactjs-monsters/static/css/main.d6d5ead2.chunk.css"
+  },
+  {
+    "revision": "12240f73676d8f2fbd85",
+    "url": "/reactjs-monsters/static/js/2.a3c82928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactjs-monsters/static/js/2.a3c82928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00f54dddca48188b9624",
+    "url": "/reactjs-monsters/static/js/main.a9e147c0.chunk.js"
+  },
+  {
+    "revision": "8174d3998f4fae0e0537",
+    "url": "/reactjs-monsters/static/js/runtime-main.968c31e5.js"
+  }
+]);
